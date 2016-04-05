69cf8f33b4ae542ca0984d92eeda03763a6a0649
Chris Berg (Create) <cberg@createdigital.com>
2016-04-05 16:59:47 -0400

    Initial commit - incomplete

---
+++ BetterDynamicPlaceholder/Helpers/BDPHelper.cs
@@ -0,0 +1,71 @@
+﻿using BetterDynamicPlaceholder.Pipeline;
+using Sitecore.Configuration;
+using Sitecore.Mvc.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BetterDynamicPlaceholder.Helpers
+{
+    class BDPHelper
+    {
+        private static string placeholderPattern = "_BDP";
+        private static List<string> DynamicPlaceholderList
+        {
+            get
+            {
+                return (List<string>)HttpContext.Current.Items["DynamicPlaceholders"];
+            }
+            set
+            {
+                HttpContext.Current.Items["DynamicPlaceholders"] = value;
+            }
+        }
+        public static HtmlString DynamicPlaceholder(this SitecoreHelper myScHelper, string placeholderName)
+        {
+            if (DynamicPlaceholderList == null)
+            {
+                DynamicPlaceholderList = new List<string>();
+            }
+            string dynamicPlaceholderKey = GetDynamicPlaceholderKey(myScHelper, placeholderName);
+            if (!string.IsNullOrWhiteSpace(dynamicPlaceholderKey))
+            {
+                placeholderName = dynamicPlaceholderKey;
+            }
+            if (!placeholderName.Contains(placeholderPattern + "_"))
+                DynamicPlaceholderList.Add(placeholderName);
+            return myScHelper.Placeholder(placeholderName);
+        }
+        private static string GetDynamicPlaceholderKey(SitecoreHelper myScHelper, string placeholderName)
+        {
+            string DPKey = "";
+            if (myScHelper.CurrentRendering != null && myScHelper.CurrentRendering.Parameters != null && myScHelper.CurrentRendering.Parameters.Contains(DPInsertRendering.PlaceholderKeyParam))
+            {
+                DPKey = myScHelper.CurrentRendering.Parameters[DPInsertRendering.PlaceholderKeyParam];
+            }
+            if (!string.IsNullOrEmpty(DPKey))
+            {
+                return placeholderName + placeholderPattern + "_" + DPKey;
+            }
+            bool flag = false;
+            int num = 0;
+            foreach (string current in DynamicPlaceholderList)
+            {
+                if (placeholderName == current || current.StartsWith(placeholderName + placeholderPattern))
+                {
+                    flag = true;
+                    num++;
+                }
+            }
+            if (flag)
+            {
+                placeholderName = placeholderName + placeholderPattern + num.ToString(CultureInfo.InvariantCulture);
+            }
+            return placeholderName;
+        }
+    }
+}
---
+++ BetterDynamicPlaceholder/Pipeline/InsertRendering.cs
@@ -0,0 +1,101 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.ExecutePageEditorAction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterDynamicPlaceholder.Pipeline
+{
+    public class DPInsertRendering : InsertRendering
+    {
+        public const string PlaceholderKeyParam = "DPKey";
+        public static char[] paramDelimieters = { '&', '=' };
+        public new void Process(PipelineArgs args)
+        {
+            base.Process(args);
+
+            Assert.ArgumentNotNull(args, "args");
+            if (args is ExecuteInsertRenderingArgs)
+            {
+
+                ExecuteInsertRenderingArgs eirArgs = args as ExecuteInsertRenderingArgs;
+
+                if (eirArgs != null) //skip if it's another type
+                {
+                    //TODO - see if rendering item contains properties we can look at rather than looking at a path
+                    //put on all renderings for now
+                    if (true)  //(eirArgs.RenderingItem.Paths.Path.ToLower().Contains("hearingfirst/modules") || eirArgs.RenderingItem.Paths.Path.ToLower().Contains("blog"))
+                    {
+                        int curIndex = 0;
+                        for (int i = 0; i < eirArgs.Device.Renderings.Count; i++)
+                        {
+                            RenderingDefinition rd = (RenderingDefinition)eirArgs.Device.Renderings[i];
+                            if (rd.Placeholder == eirArgs.PlaceholderKey)
+                            {
+
+                                //TODO - is position specific to the device?  What about language, etc.?
+                                if (curIndex == eirArgs.Position)
+                                {
+                                    if (!string.IsNullOrEmpty(rd.Parameters))
+                                        rd.Parameters += "&";
+                                    rd.Parameters += PlaceholderKeyParam + "=" + FindUniquePlaceholderID(eirArgs.Device.Renderings); //TODO - generate random - look for duplicate
+                                    eirArgs.Result.Settings.Parameters = rd.Parameters; //setting just in case they need to be in sync
+                                    //return;
+                                }
+                                curIndex++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static string FindUniquePlaceholderID(ArrayList renderings)
+        {
+            int placeholderNum = 500;
+            int highestPlaceholderNum = 0;
+            int placeholderNumTemp = 0;
+
+            bool parseNextVal = false;
+            foreach (RenderingDefinition rd in renderings)
+            {
+                if (rd != null && rd.Parameters != null && rd.Parameters.Contains(PlaceholderKeyParam))
+                {
+
+                    String[] paramVals = rd.Parameters.Split(paramDelimieters);
+                    placeholderNumTemp = 0;
+                    parseNextVal = false;
+                    for (int i = 0; i < paramVals.Count(); i++)
+                    {
+                        if (parseNextVal)
+                        {
+                            if (int.TryParse(paramVals[i], out placeholderNumTemp))
+                            {
+                                if (placeholderNumTemp > highestPlaceholderNum)
+                                    highestPlaceholderNum = placeholderNumTemp;
+                            }
+                        }
+                        else if (paramVals[i] == PlaceholderKeyParam)
+                        {
+                            parseNextVal = true;
+                        }
+                    }
+                }
+            }
+            if (highestPlaceholderNum > 0) //increment for the next item
+            {
+                placeholderNum = highestPlaceholderNum + 1;
+            }
+            return placeholderNum.ToString(CultureInfo.InvariantCulture);
+        }
+
+    }
+}
